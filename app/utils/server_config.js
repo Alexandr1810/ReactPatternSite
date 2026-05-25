e5600233e7f00d00bba95600c1c12d7a64af3499
Alexandr1810 <alexandrwork.1810@gmail.com>
2026-05-25 13:45:33 +0700

    Обновленная кнопка ТП и список городов

--- app/utils/server_config.js
+++ app/utils/server_config.js
@@ -1,7 +1,6 @@
 export const server_config = {
-    site_key: "yarkom",
-    site_folder: "localhost:5000",
-    api_protocol: "http", // https - на проде, http - для localhost
-    сaching_period: 0, 
-    captchaToken: "ysc1_dZkdh5QdQiV1LXAc0g7l4WR5nAZtCJGBGa3OE1ip0315a8de"
+    site_key: "yfanettarif",
+    site_folder: "yfanet-tarif.ru",
+    api_protocol: "https", // https - на проде, http - для localhost
+    сaching_period: 3600
 }
