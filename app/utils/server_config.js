e4419e8ae2cecc3b8e0b4cb82676806b80e50eb4
Alexandr1810 <alexandrwork.1810@gmail.com>
2026-05-26 20:54:22 +0700

    Stop tracking ignored files

--- app/utils/server_config.js
+++
@@ -1,6 +0,0 @@
-export const server_config = {
-    site_key: "yfanettarif",
-    site_folder: "yfanet-tarif.ru",
-    api_protocol: "https", // https - на проде, http - для localhost
-    сaching_period: 3600
-}
